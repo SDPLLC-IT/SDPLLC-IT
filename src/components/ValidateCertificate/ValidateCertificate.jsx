e4a70fe3ff719bc6e53608e5209e2aa01cf28070
SDPLLC-IT <segurosdelperullc@gmail.com>
2024-06-26 19:17:08 +0000

    Add more changes

--- src/components/ValidateCertificate/ValidateCertificate.jsx
+++ src/components/ValidateCertificate/ValidateCertificate.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import './ValidateCertificate.scss'
+import { Button } from "../Button/Button";
 
 export const ValidateCertificate = () => {
     const [resultId, setResultId] = useState("");
@@ -7,65 +8,47 @@ export const ValidateCertificate = () => {
     const [errorMessage, setErrorMessage] = useState("");
     const [successMessage, setSuccessMessage] = useState("");
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [isValidating, setIsValidating] = useState(false);
 
+    // Data retrieved from the backend DB
     const certificates = [
-        { id: "aB3cdE5FgHiJkL", company: "Empresa A" },
-        { id: "nOp2QrStUvWxYz", company: "Empresa B" },
-        { id: "5678AbCdEfGhIj", company: "Empresa C" },
-        { id: "LMnopQRsTuVwXy", company: "Empresa D" },
-        { id: "1234dEfGh5678i", company: "Empresa E" },
-        { id: "kLmNOpQrStUv9W", company: "Empresa F" },
-        { id: "xYz012345678Ab", company: "Empresa G" },
-        { id: "DfGhIjKlMnOpQr", company: "Empresa H" },
-        { id: "tUvWxYzAbCDEfG", company: "Empresa I" },
-        { id: "hIjKlM5678nOpQ", company: "Empresa J" },
-        { id: "sTuVwXyZ1234Ab", company: "Empresa K" },
-        { id: "DfGhIjKlMnOpQr", company: "Empresa L" },
-        { id: "StUvWxYzAbCDEf", company: "Empresa M" },
-        { id: "GhIjKlM5678nOp", company: "Empresa N" },
-        { id: "sTuVwXyZ1234Ab", company: "Empresa O" },
-        { id: "aB3cdE5FgHiJkL", company: "Empresa P" },
-        { id: "nOp2QrStUvWxYz", company: "Empresa Q" },
-        { id: "5678AbCdEfGhIj", company: "Empresa R" },
-        { id: "LMnopQRsTuVwXy", company: "Empresa S" },
-        { id: "1234dEfGh5678i", company: "Empresa T" },
-        { id: "kLmNOpQrStUv9W", company: "Empresa U" },
-        { id: "xYz012345678Ab", company: "Empresa V" },
-        { id: "DfGhIjKlMnOpQr", company: "Empresa W" },
-        { id: "tUvWxYzAbCDEfG", company: "Empresa X" },
-        { id: "hIjKlM5678nOpQ", company: "Empresa Y" },
-        { id: "sTuVwXyZ1234Ab", company: "Empresa Z" },
-        { id: "DfGhIjKlMnOpQr", company: "Empresa AA" },
-        { id: "StUvWxYzAbCDEf", company: "Empresa BB" },
-        { id: "GhIjKlM5678nOp", company: "Empresa CC" },
-        { id: "sTuVwXyZ1234Ab", company: "Empresa DD" },
-        { id: "aB3cdE5FgHiJkL", company: "Empresa EE" },
-        { id: "nOp2QrStUvWxYz", company: "Empresa FF" },
-        { id: "5678AbCdEfGhIj", company: "Empresa GG" },
-        { id: "LMnopQRsTuVwXy", company: "Empresa HH" },
-        { id: "1234dEfGh5678i", company: "Empresa II" },
-        { id: "kLmNOpQrStUv9W", company: "Empresa JJ" },
-        { id: "xYz012345678Ab", company: "Empresa KK" },
-        { id: "DfGhIjKlMnOpQr", company: "Empresa LL" },
-        { id: "tUvWxYzAbCDEfG", company: "Empresa MM" },
-        { id: "hIjKlM5678nOpQ", company: "Empresa NN" },
-        { id: "sTuVwXyZ1234Ab", company: "Empresa OO" },
-        { id: "DfGhIjKlMnOpQr", company: "Empresa PP" },
-        { id: "StUvWxYzAbCDEf", company: "Empresa QQ" },
-        { id: "GhIjKlM5678nOp", company: "Empresa RR" },
-        { id: "sTuVwXyZ1234Ab", company: "Empresa SS" }
+        { id: "nOp2QrStUvWxYz", company: "Minería y Construcción SAC" },
+        { id: "5678AbCdEfGhIj", company: "Industrias Metalúrgicas del Mar S.A." },
+        { id: "LMnopQRsTuVwXy", company: "Consultora Legal Hermanos Pérez" },
+        { id: "1234dEfGh5678i", company: "Transportes El Veloz EIRL" },
+        { id: "kLmNOpQrStUv9W", company: "Ingeniería Civil y Asociados S.C." },
+        { id: "xYz012345678Ab", company: "Manufacturas Textiles Rivas S.A.C." },
+        { id: "DfGhIjKlMnOpQr", company: "Laboratorios Médicos Modernos S.R.L." },
+        { id: "tUvWxYzAbxwsfG", company: "Compañía Agrícola San José Ltda." },
+        { id: "hIjKlM5678nOpQ", company: "Tecnología Avanzada Gómez & Cía." },
+        { id: "sTuVwXyZ1234Ab", company: "Productos Alimenticios Gourmet S.A." },
+        { id: "aB3cdE5FgHiJkL", company: "Black BBC Out S.A.C" },
+        { id: "nO22QrStxvWxYz", company: "Importaciones Internacionales del Peru SRL" },
+        { id: "52gopbCdEfGhIj", company: "Servicios Financieros Inversiones Concha SAC" },
+        { id: "LMnox3tfTuVwXy", company: "Distribuidora de Electrodomésticos y más S.A." },
+        { id: "6feedEfGh5678i", company: "Agencia de Publicidad Creativa Suarez EIRL" },
+        { id: "ksmNOpQxwtUv9W", company: "Consultora Ambiental Del Padro y Asociados S.C." },
+        { id: "xYz02wgtw678Ab", company: "Inversiones Inmobiliarias Rivas Pinedo S.A.C." },
+        { id: "DfGhIsssMnOpQr", company: "Clínica Dental Sonrisas Felices S.R.L." },
+        { id: "tUvWxYzAbCDEfG", company: "Almacenes y Distribuciones San José Ltda." },
+        { id: "aIjKlM49o8nOpQ", company: "Empresa de Tecnología Innovadora Gómez & Cía." }
       ];
+      
 
     const handleChange = (event) => {
         setCertificateValue(event.target.value);
+        setErrorMessage("")
+        setIsSubmitted(false)
+        setSuccessMessage("")
+        setResultId("")
     }
 
     const isCertificateValueValid = (certificateValue) => {
         if(certificateValue.length !== 14) {
-            setErrorMessage("El certificado ingresado no es correcto")
             return false;
         } else {
             const certificateId = certificates.findIndex(certificate => certificate.id === certificateValue)
+            console.log(certificateId)
             if(certificateId === -1) {
                 return false;
             }
@@ -75,30 +58,65 @@ export const ValidateCertificate = () => {
     }
 
     const handleSubmit = (event) => {
-        if(isCertificateValueValid(certificateValue)) {
-            console.log("submitting valid data")
-            setIsSubmitted(true);
+        event.preventDefault();
+        setIsSubmitted(true);
+    }
+
+    const validDate = () => {
+        const today = new Date();
+        let day = today.getDate();
+        let month = today.getMonth();
+        let year = today.getFullYear();
+
+        const formatDate = (y,m,d) => {
+            const date = new Date(y,m,d)
+            return `${date.getDate()}/${date.getMonth() + 1 < 10 ? '0' : ''}${date.getMonth() + 1}/${date.getFullYear()}`
         }
-        setIsSubmitted(false);
+
+        if (day <= 13) {
+            return formatDate(year, month, 13);
+        } else if(month < 11){
+                return formatDate(year, month + 1, 13);
+          } else {
+            return formatDate(year + 1, 0, 13);
+          }        
     }
 
     useEffect(() => {
+        if(resultId) {
+            setSuccessMessage(`El certificado es auténtico, pertenece a ${certificates[resultId].company}. Valido hasta el ${validDate()}`)
+        }
+    }, [resultId]);
+
+    useEffect(() => {
         if(isSubmitted) {
+            setIsValidating(true);
             setTimeout(() => {
-                setSuccessMessage(`El certificado es auténtico, pertenece a ${certificates[resultId].company}`)
+                if(isCertificateValueValid(certificateValue)) {
+                    setErrorMessage("");
+                    setIsValidating(false);
+                    return;
+                }
+                setErrorMessage("El certificado ingresado no es correcto")
+                setSuccessMessage("")
+                setIsValidating(false);
             }, 2000)
         }
     }, [isSubmitted])
 
     
     return(
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="certificate">Ingrese el id del certificado:</label>
-                <input id="certificate"type="text" onChange={handleChange} name="certificate" required placeholder="coloca el id del certificado"/>
-            </div>
-            {errorMessage && (<p className="message message__error">{errorMessage}</p>)}
-            {successMessage && (<p className="message message__success">{successMessage}</p>)}
-        </form>
+        <>
+            <h2>Aquí podrás validar la autenticidad del certificado SCTR</h2>
+            <form onSubmit={handleSubmit} className="form__certificate">
+                <div>
+                    <label htmlFor="certificate">Ingrese el id del certificado:</label>
+                    <input id="certificate"type="text" onChange={handleChange} name="certificate" required placeholder="Coloca el id del certificado"/>
+                </div>
+                <Button>{isValidating ? "Validando" : "Validar"}</Button>
+                {errorMessage && (<p className="message message--error">{errorMessage}</p>)}
+                {successMessage && (<p className="message message--success">{successMessage}</p>)}
+            </form>
+        </>
     );
 }
